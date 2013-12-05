1b136f2694a8433ce10443c8e63f63f3e5e2eaf6
Justin Yang <linuxjustin@gmail.com>
2013-12-05 21:45:29 +0800

    Add .inputrc file and improve videoJoin.sh to remove temp files.

--- videoJoin.sh
+++ videoJoin.sh
@@ -1,2 +1,15 @@
+# videoJoin.sh
+# Author: Justin Yang(darkgeek)
+# Download and merge splitted videos together
+
+echo "Trying to download viedo files..."
+wget -c -i "$1"
+
+echo "Merging..."
 cat "$1" | awk 'BEGIN{FS="/"} {if ($1 !~ /^#/) print $NF}' | xargs -t -i printf "file '%s'\n" {} > mylist.txt
 ffmpeg -f concat -i mylist.txt -c copy output.mp4
+
+echo "Removing temp files..."
+cat "$1" | awk 'BEGIN{FS="/"} {if ($1 !~ /^#/) print $NF}' | xargs -t -i rm {}
+rm "$1"
+rm mylist.txt
