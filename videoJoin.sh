a30e4bc363f122fcbaff34024385b9cf9e7179a4
Justin Yang <linuxjustin@gmail.com>
2014-08-18 01:00:48 +0800

    Use mencoder instead of ffmpeg to make videoJoin.sh work on Raspbian. FFmpeg seems to be deprecated on Raspbian.

--- videoJoin.sh
+++ videoJoin.sh
@@ -5,8 +5,6 @@
 # Download and merge splitted videos together
 
 EXPECTED_ARGS=1
-TMP_FILE="videos.tmp"
-JOIN_TMP_FILE="mylist.txt"
 OUTPUT_FILE="output.mp4"
 BROWSER_AGENT="firefox"
 IGNORE_LINE_PATTERN="^#"
@@ -18,29 +16,22 @@ then
 	exit 1
 fi
 
-echo "[`basename $0`] Cleaning old temp files..."
-rm "$TMP_FILE" > /dev/null 2>&1
-rm "$JOIN_TMP_FILE" > /dev/null 2>&1
-
 echo "[`basename $0`] Trying to download viedo files..."
 number=0
+video_numbers=""
 while IFS=$'\r\n' read line; 
 do 
-#	if [[ ! "$line" =~ $IGNORE_LINE_PATTERN ]] 
 	if [ $(expr "$line" : $IGNORE_LINE_PATTERN) -le 0 ]
 	then
 		wget -c -U "$BROWSER_AGENT" "$line" -O $number
-		echo $number >> "$TMP_FILE"
+		video_numbers=`echo $video_numbers $number`
 		number=$(expr $number + 1)
 	fi
 done < "$1"
 
 echo "[`basename $0`] Merging..."
-cat "$TMP_FILE" | xargs -t -I{} printf "file '%s'\n" {} > "$JOIN_TMP_FILE"
-ffmpeg -f concat -i "$JOIN_TMP_FILE" -c copy "$OUTPUT_FILE"
+mencoder  -ovc copy -oac pcm $video_numbers -o $OUTPUT_FILE
 
 echo "[`basename $0`] Removing temp files..."
-cat "$TMP_FILE" | xargs -t -I{} rm {}
-rm "$TMP_FILE"
+rm $video_numbers
 rm "$1"
-rm "$JOIN_TMP_FILE"
