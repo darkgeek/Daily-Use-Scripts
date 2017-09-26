8a2f38f5720c593674fb625df58650a29ad6dd94
Justin Yang <linuxjustin@gmail.com>
2017-09-27 01:05:03 +0800

    Add aurin and libaur.sh

---
+++ scripts/libaur.sh
@@ -0,0 +1,16 @@
+#!/bin/env bash
+#
+# Helper functions for cower
+
+WORKING_DIR=$HOME/Shares/buildzone
+
+function install_cower {
+    echo === cower is not available in your PATH, so I try to install it ===
+    git clone https://aur.archlinux.org/cower.git
+    cd cower && makepkg -scCfi
+}
+
+function test_if_cower_is_available {
+    command -v cower >/dev/null 2>&1 
+}
+
