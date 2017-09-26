30822dca085456c9602bd4f3e0c52dcff11c5ce7
Justin Yang <linuxjustin@gmail.com>
2017-09-27 01:14:25 +0800

    Should step into the working dir when installing cower.

--- scripts/libaur.sh
+++ scripts/libaur.sh
@@ -6,6 +6,7 @@ WORKING_DIR=$HOME/Shares/buildzone
 
 function install_cower {
     echo === cower is not available in your PATH, so I try to install it ===
+    cd $WORKING_DIR
     git clone https://aur.archlinux.org/cower.git
     cd cower && makepkg -scCfi
 }
