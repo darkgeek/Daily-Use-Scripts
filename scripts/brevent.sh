3660f67bbfc79e6cd49e2e208e52664d0fd9843a
Justin Yang <linuxjustin@gmail.com>
2017-10-06 09:18:24 +0000

    Call brevent script in termux proot environment

---
+++ scripts/brevent.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo start adb...
+export ANDROID_ADB_SERVER_PORT=12345
+adb devices
+
+echo start brevent server...
+adb -e shell sh /data/data/me.piebridge.brevent/brevent.sh
+
+echo stop adb...
+adb kill-server
+
