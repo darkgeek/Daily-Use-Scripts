a438122abe50a066ce5d151bd59f79b48a4730b3
Justin Yang <linuxjustin@gmail.com>
2017-10-11 16:00:19 +0000

    Once this lib is sourced, it will create $WORKING_DIR if needed.

--- scripts/libaur.sh
+++ scripts/libaur.sh
@@ -5,15 +5,15 @@
 WORKING_DIR=$HOME/Shares/buildzone
 ARCH=`uname -m`
 
+if [ ! -d "$WORKING_DIR" ]; then
+    mkdir -p $WORKING_DIR
+fi
+
 function install_cower {
     echo === cower is not available in your PATH, so I try to install it ===
     if [[ "$ARCH" == "armv7l" ]]; then
         pacman -S cower
     else
-        if [ ! -d "$WORKING_DIR" ]; then
-            mkdir -p $WORKING_DIR
-        fi
-
         cd $WORKING_DIR
         git clone https://aur.archlinux.org/cower.git
         cd cower && makepkg -scCfi
