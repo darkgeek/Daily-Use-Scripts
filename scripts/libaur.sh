7837b40adb20854fb9ea631dfa9d0671873591e2
Justin Yang <linuxjustin@gmail.com>
2017-10-11 15:54:53 +0000

    Improve scripts to support armv7l-proot environment better.

--- scripts/libaur.sh
+++ scripts/libaur.sh
@@ -3,15 +3,23 @@
 # Helper functions for cower
 
 WORKING_DIR=$HOME/Shares/buildzone
+ARCH=`uname -m`
 
 function install_cower {
     echo === cower is not available in your PATH, so I try to install it ===
-    cd $WORKING_DIR
-    git clone https://aur.archlinux.org/cower.git
-    cd cower && makepkg -scCfi
+    if [[ "$ARCH" == "armv7l" ]]; then
+        pacman -S cower
+    else
+        if [ ! -d "$WORKING_DIR" ]; then
+            mkdir -p $WORKING_DIR
+        fi
+
+        cd $WORKING_DIR
+        git clone https://aur.archlinux.org/cower.git
+        cd cower && makepkg -scCfi
+    fi
 }
 
 function test_if_cower_is_available {
     command -v cower >/dev/null 2>&1 
 }
-
